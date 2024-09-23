c2bbc02be404837f17fddb4eb3c599d0f16a7e69
samit <desmukhkumarsamit@gmail.com>
2024-09-23 22:45:34 +0530

    modify

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-// Create a new Sequelize instance for SQL Server connection
 const sequelize = new Sequelize('database_name', 'username', 'password', {
   host: 'localhost',
   dialect: 'mssql',
@@ -11,5 +10,6 @@ const sequelize = new Sequelize('database_name', 'username', 'password', {
     }
   }
 });
+
 
 module.exports = sequelize;
