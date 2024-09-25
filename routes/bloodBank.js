a9a0c36747349ab1eaf3638dd7d0f9098ad4361e
samit <desmukhkumarsamit@gmail.com>
2024-09-25 23:37:00 +0530

    Update a blood bank by ID

--- routes/bloodBank.js
+++ routes/bloodBank.js
@@ -37,3 +37,34 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Update a blood bank by ID
+router.put('/update/:id', async (req, res) => {
+  const {
+    name, licence, address, phone, altPhone, profileImage, openTime, closeTime, others
+  } = req.body;
+
+  try {
+    let bloodBank = await BloodBank.findByPk(req.params.id);
+
+    if (!bloodBank) {
+      return res.status(404).json({ msg: 'Blood bank not found' });
+    }
+
+    bloodBank.name = name;
+    bloodBank.licence = licence;
+    bloodBank.address = address;
+    bloodBank.phone = phone;
+    bloodBank.altPhone = altPhone;
+    bloodBank.profileImage = profileImage;
+    bloodBank.openTime = openTime;
+    bloodBank.closeTime = closeTime;
+    bloodBank.others = others;
+
+    await bloodBank.save();
+    res.json(bloodBank);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
